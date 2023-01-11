55d3fae954427e129b820bc2de7eddb9f1929e94
ppedvLukasK <lukask@ppedv.de>
2023-01-11 11:41:37 +0100

    M007

---
+++ M007/Fenster.cs
@@ -0,0 +1,119 @@
+﻿namespace M007.Bauteile
+{
+	internal class Fenster
+	{
+		#region Variable + Get/Set
+		private double laenge; //Felder sollten nicht von außen angreifbar sein (nur über Methoden)
+
+		/// <summary>
+		/// Gibt die Länge des Fensters zurück.
+		/// </summary>
+		/// <returns>Die Länge des Fenster in Meter</returns>
+		public double GetLaenge()
+		{
+			return laenge;
+		}
+
+		/// <summary>
+		/// Setzt die Länge des Fenster auf einen neuen Wert.
+		/// </summary>
+		/// <param name="laenge">Die neue Länge des Fensters in Meter (0 bis 10)</param>
+		public void SetLaenge(double laenge)
+		{
+			if (laenge > 0 && laenge < 10) //Überprüfung machen bevor er gesetzt wird
+				this.laenge = laenge; //this: Aus der Methode herausgreifen (nach oben greifen)
+			else
+				Console.WriteLine("Länge ist zu klein/groß");
+		}
+		#endregion
+
+		#region Properties
+		private double breite;
+
+		public double Breite
+		{
+			get => breite; //{ return breite; }
+			set //Hier ist auch Überprüfungscode möglich
+			{
+				if (value > 0 && value < 10)
+					breite = value; //value: Der neue Wert (wie oben in der Set Methode -> laenge)
+				else
+					Console.WriteLine("Breite ist zu klein/groß");
+			} //value kommt von der Main Methode bei der Zuweisung (f.Breite = 4)
+		}
+
+		//Get-Only Property
+		//public double Area
+		//{
+		//	get
+		//	{
+		//		return laenge * breite;
+		//	}
+		//}
+
+		//Kurzform von obigem Get-Only Property
+		public double Area => laenge * breite;
+
+		//Funktion statt Property
+		public double CalcArea() => laenge * breite;
+
+		private FensterStatus status = FensterStatus.Geschlossen;
+
+		public FensterStatus Status
+		{
+			get => status;
+			private set => status = value; //private set: nicht von außen setzbar (nur innerhalb der Klasse)
+		}
+
+		/// <summary>
+		/// Eine Methode die das Fenster öffnet
+		/// </summary>
+		public void FensterOeffnen()
+		{
+			if (Status != FensterStatus.Offen)
+				Status = FensterStatus.Offen; //hier private set sichtbar
+			else
+				Console.WriteLine("Fenster ist bereits geöffnet");
+		}
+
+		private string Farbe = "Grau"; //Standardwert setzen, wird bei Erstellung des Objekts gesetzt (new)
+
+		public int Scheibenanzahl { get; set; } = 2; //Standardwert setzen, wird bei Erstellung des Objekts gesetzt (new)
+		#endregion
+
+		public Fenster() { }
+
+		public Fenster(double laenge, double breite) //Konstruktor: Code der bei Erstellung (new) des Objekts ausgeführt wird
+		{
+			SetLaenge(laenge);
+			Breite = breite;
+		}
+
+		public Fenster(double laenge, double breite, int scheiben) : this(laenge, breite) //Konstruktoren verketten (Wenn dieser Konstruktor verwendet wird, wird der obere auch involviert)
+		{
+			Scheibenanzahl = scheiben;
+		}
+
+		~Fenster() //~ + Tab + Tab
+		{
+			Console.WriteLine($"Fenster eingesammelt {GetHashCode()}"); //Wird aufrufen wenn der Garbage Collector das Objekt einsammelt
+			//GetHashCode(): Zeigt die Speicheradresse
+			//Bei Objektvergleichen mit == werden die Speicheradressen verglichen
+		}
+
+		public static int Zaehler;
+
+		public static void InkrementZaehler()
+		{
+			Zaehler++;
+			//Console.WriteLine(laenge); //nicht möglich
+		}
+	}
+
+	enum FensterStatus
+	{
+		Geschlossen,
+		Offen,
+		Gekippt
+	}
+}
---
+++ M007/Program.cs
@@ -0,0 +1,86 @@
+﻿using M007.Bauteile;
+
+namespace M007;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		#region GC
+		for (int i = 0; i < 5; i++)
+		{
+			Fenster f = new Fenster();
+		}
+
+		GC.Collect(); //Hier GC erzwingen
+		GC.WaitForPendingFinalizers(); //Warte auf alle Destruktoren
+		#endregion
+
+		#region Static
+		//Statische Member müssen ohne Objekt arbeiten
+		//Console c = new Console();
+		//c.WriteLine(); nicht möglich, da statisch
+		//Console.WriteLine();
+
+		Fenster fenster = new Fenster();
+		//Fenster.FensterOeffnen(); nicht möglich, da kein Objekt existiert
+		fenster.FensterOeffnen();
+
+		//fenster.Zaehler++; nicht möglich
+		Fenster.Zaehler++;
+		Fenster.InkrementZaehler();
+		#endregion
+
+		#region Werte-/Referenztypen
+		//class
+		//Referenztyp
+		//==, != werden Speicheradressen verglichen
+		//Zuweisungen haben Referenzen anstatt Kopien
+
+		//struct
+		//Wertetyp
+		//==, != werden die Inhalte verglichen
+		//Zuweisungen werden kopiert statt referenziert
+
+		//Wertetyp
+		int original = 5;
+		int x = original;
+		original = 10;
+
+		//Referenztyp
+		Fenster f1 = new();
+		Fenster f2 = f1; //Referenz zu f1
+		f1.FensterOeffnen(); //f1 und f2 zeigen auf das gleiche Objekt im RAM
+		#endregion
+
+		#region Null
+		Fenster f3; //Standardmäßig null (es hängt kein Wert daran)
+		f3 = null; //Referenz zum Objekt trennen
+		//f3.FensterOeffnen(); Nicht vorhandenes Objekt kann nicht geöffnet werden
+
+		if (f3 != null) //Schauen ob das Objekt existiert
+		{
+			f3.FensterOeffnen();
+		}
+
+		f3?.FensterOeffnen(); //Schneller Null-Check: wenn das Objekt null ist, führe den Code danach nicht aus
+
+		if (f3 == null)
+		{
+			f3 = new();
+		}
+
+		f3 ??= new(); //Wenn das Objekt null ist, führe den Code aus, sonst mach nichts
+
+		if (f3 is null || f3 is not null) //Selbiges wie == und !=
+		{
+
+		}
+		#endregion
+	}
+}
+
+public class WrappedInt
+{
+	int RefInt; //"struct referenzierbar machen"
+}
