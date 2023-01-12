1302c33b6450a397f88ed2ad594c5c8bbb736906
ppedvLukasK <lukask@ppedv.de>
2023-01-12 13:37:50 +0100

    M012

--- M012/LabCode.cs
+++ M012/LabCode.cs
@@ -39,30 +39,30 @@ public class Fahrzeug
 	public int ID;
 	public int MaxGeschwindigkeit;
 	public FahrzeugMarke Marke;
-	public List<Sitzplatz> Sitze;
+	public List<Sitzplatz> SitzeListe;
 
 	public Fahrzeug(int id, int v, FahrzeugMarke fm)
 	{
 		ID = id;
 		MaxGeschwindigkeit = v;
 		Marke = fm;
-		Sitze = new();
+		SitzeListe = new();
 
 		//Anzahl Sitzplätze anhand der Geschwindigkeit (6: max 150km/h, 5 bis 250km/h, 4 ab 250km/h)
 		int sitze = v <= 150 ? 6 : v <= 250 ? 5 : 4;
 
 		//Sitzplätze erstellen
 		for (int i = 0; i < sitze; i++)
-			Sitze.Add(new Sitzplatz());
+			SitzeListe.Add(new Sitzplatz());
 
 		//Sitzplätze semi-zufällig belegen damit die Übung zwischen Teilnehmern gleiche Ergebnisse liefert
 		//Geschwindigkeit modulo Anzahl Sitzplätze besetzen
 		for (int i = 0; i < v % (sitze + 1); i++)
-			Sitze[i].IstBesetzt = true;
+			SitzeListe[i].IstBesetzt = true;
 	}
 }
 
-public record Sitzplatz
+public class Sitzplatz
 {
 	public bool IstBesetzt;
 }
